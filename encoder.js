2fa2a2c995ff80553bb055645b2842588170b870
Ahmoray Arana <bebegirl217@gmail.com>
2016-10-12 09:11:03 -0700

    Echoing

---
+++ encoder.js
@@ -0,0 +1,51 @@
+var letters = {
+  65:" a",
+  66: "b",
+  67: "c",
+  68: "d",
+  69: "e",
+  70: "f",
+  71: "g",
+  72: "h",
+  73: "i",
+  74: "j",
+  75: "k",
+  76: "l",
+  77: "m",
+  78: "n",
+  79: "o",
+  80: "p",
+  81: "q",
+  82: "r",
+  83: "s",
+  84: "t",
+  85: "u",
+  86: "v",
+  87: "w",
+  88: "x",
+  89: "y",
+  90: "z",
+}
+
+$ ("#inputArea").keydown(function (e) {
+
+  if($("input:checked").val() == "echo") {
+    $("#textArea").append(letters[e.keyCode]);
+  }
+  //allowing the echoing text to work only when the echo button is clicked
+  else if ($("input:checked").val() == "caesar cipher") {
+
+  }
+
+  else if($("input:checked").val() == "heiroglyphics") {
+
+  }
+
+});
+//Allowing the buttons to echo
+
+$("input[type='radio']").click(function() {
+  $("input:checked").prop('checked', false);
+  $(this).prop('checked', true)
+})
+//Allowing a user to move each of the buttons
